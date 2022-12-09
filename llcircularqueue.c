6e145944ed6c1e5a690afd6bd0274ca4b3204d62
Roshin James <98834015+roshnjames@users.noreply.github.com>
2022-12-09 15:13:16 +0530

    Add files via upload

---
+++ llcircularqueue.c
@@ -0,0 +1,94 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+  int data;
+  struct node *next;
+};
+ struct node *front=NULL,*rear=NULL,*ptr,*newnode;
+
+ void enqueue(void);
+ void dequeue(void);
+ void display(void);
+
+ void main()
+ {
+   int op;
+   do{
+     printf("Enter your Option!\n1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n_____________________option :");
+     scanf("%d",&op);
+     switch(op){
+       case 1:printf("Enqueue.....\n");
+              enqueue();
+              break;
+      case 2:printf("Dequeue....\n");
+             dequeue();
+             break;
+      case 3:printf("Displaying the Queue elements......\n");
+             display();
+             break;
+      default:printf("Invalid option\n");
+             break;
+     }
+   }while(op!=4);
+ }
+
+
+
+ void enqueue()
+ {
+   newnode=(struct node*)malloc(sizeof(struct node));
+   if((front==NULL)&&(rear==NULL)){
+     printf("Enter data\n");
+     scanf("%d",&newnode->data);
+     newnode->next=front;
+     front=newnode;
+     rear=newnode;
+   }
+   else {
+     printf("Enter data\n");
+     scanf("%d",&newnode->data);
+     rear->next=newnode;
+     rear=newnode;
+     rear->next=front;
+   }
+ }
+
+
+ void dequeue()
+ {
+   if((front==NULL)&&(rear==NULL))
+   {
+     printf("Queue is empty\n");
+   }
+   else if(front==rear){
+   free(front);
+   front=NULL;
+   rear=NULL;
+   }
+   else{
+     ptr=front;
+     front=front->next;
+     free(ptr);
+     printf("deleted successfully\n");
+     ptr=front;
+     rear->next=front;
+ }}
+
+
+ void display()
+ {
+   if((front==NULL)&&(rear==NULL))
+   {
+     printf("Queue is empty\n");
+   }
+   else{
+     ptr=front;
+       printf("%d\t",ptr->data);
+       ptr=ptr->next;
+     while(ptr!=front){
+       printf("%d\t",ptr->data);
+       ptr=ptr->next;
+     }printf("\n");
+   }
+ }
